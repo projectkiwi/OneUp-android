f9972f2d2a820a27199fc107fbbffce4b6bd1071
Adam Loeb <loeb@purdue.edu>
2016-03-04 00:35:25 -0500

    Added beginning of challenge detail page stuff.

--- OneUp/app/src/main/java/com/purduecs/kiwi/oneup/ChallengeDetailActivity.java
+++ OneUp/app/src/main/java/com/purduecs/kiwi/oneup/ChallengeDetailActivity.java
@@ -8,9 +8,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.purduecs.kiwi.oneup.models.Challenge;
+import com.purduecs.kiwi.oneup.web.ChallengeWebRequest;
+import com.purduecs.kiwi.oneup.web.RequestHandler;
 
 public class ChallengeDetailActivity extends AppCompatActivity {
 
@@ -23,12 +26,42 @@ public class ChallengeDetailActivity extends AppCompatActivity {
     }
 
     Challenge mChallenge;
+    ImageView mMedia;
+    TextView mTitle, mWinner, mDesc, mCategories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_detail);
 
+        mMedia = (ImageView) findViewById(R.id.challenge_media);
+        mTitle = (TextView) findViewById(R.id.challenge_name);
+        mWinner = (TextView) findViewById(R.id.challenge_winner);
+        mDesc = (TextView) findViewById(R.id.challenge_desc);
+        mCategories = (TextView) findViewById(R.id.challenge_categories);
+
         String challengeId = getIntent().getStringExtra(EXTRA_CHALLENGE_ID);
+
+        new ChallengeWebRequest(challengeId, new RequestHandler<Challenge>() {
+            @Override
+            public void onSuccess(Challenge response) {
+                mChallenge = response;
+                mTitle.setText(mChallenge.name);
+                mWinner.setText(mChallenge.owner);
+                mDesc.setText(mChallenge.desc);
+
+                String categories = "";
+                for (int i = 0; i < mChallenge.categories.length; i++) {
+                    categories += mChallenge.categories[i];
+                    categories += ", ";
+                }
+                categories = categories.substring(0, categories.length()-2);
+                mCategories.setText(categories);
+            }
+
+            @Override
+            public void onFailure() {
+            }
+        });
     }
 }
---
+++ OneUp/app/src/main/java/com/purduecs/kiwi/oneup/web/ChallengeWebRequest.java
@@ -0,0 +1,59 @@
+package com.purduecs.kiwi.oneup.web;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.purduecs.kiwi.oneup.OneUpApplication;
+import com.purduecs.kiwi.oneup.R;
+import com.purduecs.kiwi.oneup.models.Challenge;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Adam on 3/3/16.
+ */
+public class ChallengeWebRequest implements OneUpWebRequest<JSONObject, Challenge> {
+
+    public ChallengeWebRequest(String challengeId, final RequestHandler<Challenge> handler) {
+
+        // Now get that object
+        Request request = new JsonObjectRequest(Request.Method.GET,
+                OneUpWebRequest.BASE_URL + "/challenges/" + challengeId,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        handler.onSuccess(parseResponse(response));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                handler.onFailure();
+            }
+        });
+
+        RequestQueueSingleton.getInstance(OneUpApplication.getAppContext()).addToRequestQueue(request);
+    }
+
+    @Override
+    public Challenge parseResponse(JSONObject response) {
+        Challenge c = new Challenge();
+        try {
+            c.id = response.getString("_id");
+            c.name = response.getString("name");
+            c.image = R.drawable.doge_with_sunglasses;
+            c.categories = response.getJSONArray("categories").toString()
+                    .replace("\"", "").replace("[", "").replace("]", "").split(",");
+            c.owner = "temp";
+            c.score = 164;
+            c.time = 9;
+            c.desc = "lots of placeholder text yo so this looks like a pretty high quality description";
+        } catch (Exception e) {
+            Log.e("HEY", "Had an issue parsing JSON when getting individual challenge in ChallengeWebRequest");
+        }
+        return c;
+    }
+}
