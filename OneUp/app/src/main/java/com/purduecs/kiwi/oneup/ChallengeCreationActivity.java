af3fe672e255e5730e87522f92fb2fb09c268233
Adam Loeb <loeb@purdue.edu>
2016-03-04 10:55:01 -0500

    Did a bunch of image adding for the upload a challenge page.

--- OneUp/app/src/main/java/com/purduecs/kiwi/oneup/ChallengeCreationActivity.java
+++ OneUp/app/src/main/java/com/purduecs/kiwi/oneup/ChallengeCreationActivity.java
@@ -6,6 +6,9 @@ package com.purduecs.kiwi.oneup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class ChallengeCreationActivity extends AppCompatActivity {
 
@@ -15,5 +18,14 @@ public class ChallengeCreationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_challenge_creation);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_challenge_creation, menu);
+        return true;
+    }
 
+    public void uploadChallenge(MenuItem menuItem) {
+
+    }
 }
